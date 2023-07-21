e0c6077113702a133b50e43f8b96cb997b83fc5e
Michael H. Vogt <michael.vogt@progwise.net>
2023-07-21 17:35:51 +0200

    click and wait

--- apps/playwright-scraper/src/main.ts
+++ apps/playwright-scraper/src/main.ts
@@ -1,6 +1,5 @@
 import playwright from 'playwright'
 
-
 const doScrape = async () => 
 {
   const browser = await playwright['chromium'].launch({headless: false})
@@ -10,11 +9,19 @@ const doScrape = async () =>
   const acceptCookies = await page.getByText(/alle akzeptieren/i);
   await Promise.all((await acceptCookies.all()).map(async button => button.click()))
   const links = await page.getByRole('link').all()
+  await page.waitForTimeout(2000)
+  const originalPageUrl = page.url()
   
   for(let i=0;i<links.length;i++) {
-    await links[i].click()
-    await page.waitForTimeout(500)
-    await page.goBack()
+    const textContent = await links[i].textContent()
+    if(textContent && textContent?.trim().length<1) {
+      await links[i].click()
+      await page.waitForTimeout(500)
+      if (page.url() !== originalPageUrl) {
+        await page.goBack()
+
+      }
+    }
   }
   await browser.close()
 }
