d5409196253f205ef5b30790daa472521f583af5
Michael H. Vogt <michael.vogt@progwise.net>
2023-07-24 09:35:35 +0200

    less hangs but still...

--- apps/playwright-scraper/src/main.ts
+++ apps/playwright-scraper/src/main.ts
@@ -10,27 +10,32 @@ export interface ScrapeResult {
 
 const acceptCookies = async (page: playwright.Page) => {
   try {
-    const acceptCookies = await page.getByText(/alle akzeptieren/i);
-    await Promise.all((await acceptCookies.all()).map(async button => button.click()))
+    const acceptCookies = await page.getByText(/alle akzeptieren/i).getByRole('button');
+    const buttons = (await acceptCookies.all())
+    if (buttons.length > 0) {
+      const isVisible = await buttons[0].isVisible()
+      if (isVisible) {
+        await buttons[0].click()
+      }
+    }
   }
   catch(e) {
     console.error('Error accepting cookies', e)
   }
 }
 
-const scrapePage = async (url: string): Promise<ScrapeResult> => {
-  const browser = await playwright['chromium'].launch({headless: false})
-  const context = await browser.newContext()
+const scrapePage = async (url: string, context: playwright.BrowserContext): Promise<ScrapeResult> => {
+
   const page = await context.newPage()
-  await page.goto(url, {waitUntil: 'networkidle'})
+  await new Promise(x => setTimeout(x, 1000));
+  await page.goto(url)
   await acceptCookies(page)
   const pageTitle = await page.title()
   const body = await page.locator('main')
   const linkLocators = await page.locator('a').all()
   const links = await Promise.all(linkLocators.map(ll => ll.getAttribute('href')))
   const texts = pageTitle + '\n\n' + (await body.allTextContents()).map(text => text.replace(/\s\s+/g, ' ')).join('\n')
-  
-  await browser.close()
+  await page.close()
   return {
     url,
     summary: `summary for ${pageTitle}`, // await getServiceSummary(texts),
@@ -40,6 +45,8 @@ const scrapePage = async (url: string): Promise<ScrapeResult> => {
 }
 
 const doScrape = async (url: string): Promise<Array<ScrapeResult>> => {
+  const browser = await playwright['chromium'].launch({headless: false})
+  const context = await browser.newContext()
   const urls = [url]
   const urlsDone: Array<string> =[]
   const results: Array<ScrapeResult> = []
@@ -47,11 +54,13 @@ const doScrape = async (url: string): Promise<Array<ScrapeResult>> => {
   while (urlsTodo.length > 0) {
     urlsDone.push(urlsTodo[0])
     console.log(`scraping ${urlsTodo[0]}`)
-    const result = await scrapePage(urlsTodo[0])
+    const result = await scrapePage(urlsTodo[0], context)
     urlsTodo = Array.from(new Set([...urlsTodo, ...result.links].filter(url => !urlsDone.includes(url))))
     console.log(`-- scraped ${urlsTodo[0]}`)
     results.push(result)
   }
+
+  await browser.close()
   return results
 }
 
