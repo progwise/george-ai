a82178ac3c38cc8352caade0f96d4d1333c7c7c8
Michael H. Vogt <michael.vogt@progwise.net>
2023-08-09 16:00:05 +0200

    added pothos for graphql schema generate

--- apps/nextjs-web/app/graphql/route.ts
+++ apps/nextjs-web/app/graphql/route.ts
@@ -1,22 +1,11 @@
 // Next.js Custom Route Handler: https://nextjs.org/docs/app/building-your-application/routing/router-handlers
-import { createSchema, createYoga } from 'graphql-yoga'
- 
+import { schema } from '@george-ai/pothos-graphql'
+ import { createYoga } from 'graphql-yoga'
 const { handleRequest } = createYoga({
-  schema: createSchema({
-    typeDefs: /* GraphQL */ `
-      type Query {
-        greetings: String
-      }
-    `,
-    resolvers: {
-      Query: {
-        greetings: () => 'This is the `greetings` field of the root `Query` type'
-      }
-    }
-  }),
+  schema,
  
   // While using Next.js file convention for routing, we need to configure Yoga to use the correct endpoint
-  graphqlEndpoint: '/api/graphql',
+  graphqlEndpoint: '/graphql',
  
   // Yoga needs to know how to create a valid Next response
   fetchAPI: { Response }
