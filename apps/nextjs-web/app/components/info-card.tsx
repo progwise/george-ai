2c8c104f1f16fd99c6962b4514827177fea6ab07
Tony Schroder <tonyschroder@Tonys-MBP.fritz.box>
2023-08-11 17:25:40 +0200

    first working version

--- apps/nextjs-web/app/components/info-card.tsx
+++ apps/nextjs-web/app/components/info-card.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image'
+import { Page } from '../page'
 
-export const InfoCard = () => {
+export const InfoCard = ({ page }: { page: Page }) => {
+  console.log('typen', typeof page.webPageSummaries[0].generatedKeywords)
   return (
-    <div className="flex flex-col gap-5">
-      <span className="border-b border-black">
-        ich habe folgende Informationen für Sie gefunden:
-      </span>
+    <div className="flex flex-col gap-5 border-2 p-8 rounded-md">
       <div className="flex justify-between">
         <div className="flex gap-2 items-center">
-          <span className="font-bold text-lg">Urlaubsgeld</span>
+          <span className="font-bold text-lg">{page.title}</span>
           <div className="border border-black rounded-md px-6 bg-slate-100">
             draft
           </div>
@@ -37,26 +36,29 @@ export const InfoCard = () => {
       </div>
 
       <div>
-        <span>
-          Die Springfield AG gewährte in den vergangenen Jahren regelmäßig
-          Urlaubsgeld. Einen Antrag dazu müssen Sie nicht stellen.
+        <span className="line-clamp-3">
+          {page.webPageSummaries[0].generatedSummary}
         </span>
       </div>
       <div>
         <span>Quelle: </span>
-        <a
-          className="text-blue-500"
-          href="http//intranet.springfield-ag.com/personal/urlaub"
-          target="_blank"
-        >
-          http//intranet.springfield-ag.com/personal/urlaub
+        <a className="text-blue-500" href={page.url} target="_blank">
+          {page.url}
         </a>
       </div>
       <div className="flex items-center justify-between">
-        <div className="flex gap-2">
-          <div className="border border-black rounded-md px-6 bg-slate-100">
-            Personal
-          </div>
+        <div className="flex flex-wrap gap-2">
+          {JSON.parse(page.webPageSummaries[0].generatedKeywords).map(
+            (keyword: string, index: string) => (
+              <div
+                key={index}
+                className="border border-black rounded-md px-6 bg-slate-100"
+              >
+                {keyword}
+              </div>
+            ),
+          )}
+
           <div className="border border-black rounded-md px-6 bg-slate-100">
             Allgemeines
           </div>
@@ -64,9 +66,11 @@ export const InfoCard = () => {
             Urlaub
           </div>
         </div>
-        <button className="border border-black rounded-md px-6 text-xl bg-slate-100">
-          Details...
-        </button>
+        <div className="">
+          <button className="border border-black rounded-md px-6 text-xl bg-slate-100">
+            Details...
+          </button>
+        </div>
       </div>
     </div>
   )
--- apps/nextjs-web/app/page.tsx
+++ apps/nextjs-web/app/page.tsx
@@ -3,13 +3,66 @@ import { Header } from './components/header'
 import { SearchBox } from './components/search-box'
 import { InfoCard } from './components/info-card'
 
-export default function Home() {
+interface WebPageSummary {
+  id: string
+  generatedKeywords: string
+  generatedSummary: string
+  largeLanguageModel: string
+}
+
+export interface Page {
+  title: string
+  url: string
+  webPageSummaries: WebPageSummary[]
+}
+
+export default async function Home() {
+  let pages: Page[] = []
+
+  try {
+    const resonse = await fetch('http://localhost:3000/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${process.env.STRAPI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        query: `
+      query GetScrapedWebPages {
+        allPages {
+          title
+          url
+          webPageSummaries {
+            id
+            generatedKeywords
+            generatedSummary
+            largeLanguageModel
+          }
+        }
+      }
+      `,
+      }),
+    })
+    const respon = await resonse.json()
+    pages = respon.data.allPages
+    console.log('pages:', pages)
+  } catch (error) {
+    console.error(error)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 ">
       <div className="max-w-3xl flex flex-col gap-5">
         <Header />
         <SearchBox />
-        <InfoCard />
+        <span className="border-b border-black">
+          ich habe folgende Informationen für Sie gefunden:
+        </span>
+        {pages.map((page, index) => (
+          <div key={index}>
+            <InfoCard page={page} />
+          </div>
+        ))}
       </div>
     </main>
   )
