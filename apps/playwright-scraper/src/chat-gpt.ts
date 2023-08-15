5e1bbdf7b344bcb5f254b71f08890903432bb65e
Tony Schroder <tonyschroder@Tonys-MBP.fritz.box>
2023-08-15 09:20:10 +0200

    abb try to getKeywords

--- apps/playwright-scraper/src/chat-gpt.ts
+++ apps/playwright-scraper/src/chat-gpt.ts
@@ -38,48 +38,54 @@ export const getServiceSummary = async (content: string, language: string) => {
 
     return responseAsString
   } catch (error) {
-    // @ts-ignore
     console.error('error using chatGPT')
     console.log(JSON.stringify(error, undefined, 2))
   }
 }
 
 export const getKeywords = async (content: string, language: string) => {
-  let instruction = ''
-  if (language === 'de') {
-    instruction =
-      'Erzeuge eine List, die die wichtigsten META Keywords für SEO für den Text, die der User sendet, enthält, in Deutsch.'
-  } else if (language === 'en') {
-    instruction =
-      'Generate a list containing the most important META keywords for SEO from the text the user provides, in english.'
+  try {
+    let instruction = ''
+    if (language === 'de') {
+      instruction =
+        'Erzeuge eine List, die die wichtigsten META Keywords für SEO für den Text, die der User sendet, enthält, in Deutsch.'
+    } else if (language === 'en') {
+      instruction =
+        'Generate a list containing the most important META keywords for SEO from the text the user provides, in english.'
+    }
+
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: instruction,
+        },
+        {
+          role: 'system',
+          content: 'The list should contain 10 entries, sorted by importance.',
+        },
+        {
+          role: 'system',
+          content:
+            "The list should be in the format: 'Keyword1, Keyword2, Keyword3, ..., Keyword10'.",
+        },
+        {
+          role: 'system',
+          content: 'Reply only with the 10 keywords, without any prefix.',
+        },
+        { role: 'user', content },
+      ],
+    })
+
+    const responseAsString = response.data.choices.at(0)?.message?.content
+    const keywords = responseAsString
+      ?.split(',')
+      .map((keyword) => keyword.trim())
+
+    return keywords
+  } catch (error) {
+    console.error('Error using chatGPT while fetching keywords')
+    console.log(JSON.stringify(error, undefined, 2))
   }
-
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'system',
-        content: instruction,
-      },
-      {
-        role: 'system',
-        content: 'The list should contain 10 entries, sorted by importance.',
-      },
-      {
-        role: 'system',
-        content:
-          "The list should be in the format: 'Keyword1, Keyword2, Keyword3, ..., Keyword10'.",
-      },
-      {
-        role: 'system',
-        content: 'Reply only with the 10 keywords, without any prefix.',
-      },
-      { role: 'user', content },
-    ],
-  })
-
-  const responseAsString = response.data.choices.at(0)?.message?.content
-  const keywords = responseAsString?.split(',').map((keyword) => keyword.trim())
-
-  return keywords
 }
