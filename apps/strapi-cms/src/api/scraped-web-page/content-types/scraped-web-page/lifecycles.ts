44bf22f35a6d7e6a2860f3f5b022c6e8b5d766cb
Tony Schroder <tonyschroder@Tonys-MBP.fritz.box>
2023-08-29 11:02:21 +0200

    create a webhook

---
+++ apps/strapi-cms/src/api/scraped-web-page/content-types/scraped-web-page/lifecycles.ts
@@ -0,0 +1,11 @@
+// import { updateTypesenseDocument } from "@george-ai/typesense-client";
+
+
+// export default  {
+//   afterCreate(event) {
+//     updateTypesenseDocument(event.params.data)
+//   },
+//   afterUpdate(event) {
+//     updateTypesenseDocument(event.params.data)
+//   },
+// };
