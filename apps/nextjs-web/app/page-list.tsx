e8f074b370d7bb7fd4b35f8705e580f82b059e65
Tony Schroder <http://www.tony-schroeder.de>
2023-09-21 12:03:24 +0200

    add useEffect

--- apps/nextjs-web/app/page-list.tsx
+++ apps/nextjs-web/app/page-list.tsx
@@ -1,40 +1,58 @@
+'use client'
+
 import { InfoCard } from './components/info-card/info-card'
 import { graphql } from '@/src/gql'
 import { FilterSelectionProps } from './components/filter-selection/filter-selection'
 import { getClient } from './client/urql-client'
+import { useEffect, useState } from 'react'
+import { GetSearchWebPagesQuery } from '@/src/gql/graphql'
 
 interface PageListProps extends FilterSelectionProps {
   query?: string
 }
 
-export async function PageList({ query, lang, status, llm }: PageListProps) {
-  const result = await getClient().query(
-    graphql(`
-      query GetSearchWebPages(
-        $query: String
-        $language: [String!]
-        $publicationState: [String!]
-        $largeLanguageModel: [String!]
-      ) {
-        searchResult(
-          query: $query
-          language: $language
-          publicationState: $publicationState
-          largeLanguageModel: $largeLanguageModel
-        ) {
-          id
-          ...InfoCard
-        }
+export function PageList({ query, lang, status, llm }: PageListProps) {
+  const [pages, setPages] = useState<
+    GetSearchWebPagesQuery['searchResult'] | null
+  >()
+
+  useEffect(() => {
+    const executeQuery = async () => {
+      try {
+        const result = await getClient().query(
+          graphql(`
+            query GetSearchWebPages(
+              $query: String
+              $language: [String!]
+              $publicationState: [String!]
+              $largeLanguageModel: [String!]
+            ) {
+              searchResult(
+                query: $query
+                language: $language
+                publicationState: $publicationState
+                largeLanguageModel: $largeLanguageModel
+              ) {
+                id
+                ...InfoCard
+              }
+            }
+          `),
+          {
+            query,
+            language: lang,
+            publicationState: status,
+            largeLanguageModel: llm,
+          },
+        )
+        setPages(result.data?.searchResult)
+      } catch (error) {
+        console.error(error)
       }
-    `),
-    {
-      query,
-      language: lang,
-      publicationState: status,
-      largeLanguageModel: llm,
-    },
-  )
-  const pages = result.data?.searchResult
+    }
+
+    executeQuery()
+  }, [query, lang, status, llm])
 
   return (
     <>
