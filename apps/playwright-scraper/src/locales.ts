b269c733a3d2c1cf63448b66022931f610ac62af
Michael H. Vogt <michael.vogt@progwise.net>
2023-09-22 16:07:41 +0200

    typescript versions and localhost bugfix

--- apps/playwright-scraper/src/locales.ts
+++ apps/playwright-scraper/src/locales.ts
@@ -2,7 +2,6 @@ import { graphql } from './gql'
 import { strapiClient } from './strapi-client'
 
 export const getStrapiLocales = async () => {
-  try {
     const { i18NLocales } = await strapiClient.request(
       graphql(`
         query GetAllLocales {
@@ -24,8 +23,4 @@ export const getStrapiLocales = async () => {
         ?.map((locale) => locale.attributes?.code)
         .filter(Boolean) as string[]) || []
     return localeCodes
-  } catch (error) {
-    console.error('Error fetching Strapi locales:', error)
-    return []
-  }
 }
