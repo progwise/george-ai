30e96f19856a1a41e8d6094b8729ff9952266246
Tony Schroder <http://www.tony-schroeder.de>
2023-10-20 10:25:30 +0200

    add vitest.config

--- packages/strapi-client/src/prompts/get-default-prompt-ids.test.ts
+++ packages/strapi-client/src/prompts/get-default-prompt-ids.test.ts
@@ -1,11 +1,6 @@
 import { graphql } from 'msw'
 import { server } from '../mocks/server'
 import { getDefaultPromptIds } from './get-default-prompt-ids'
-import { afterAll, afterEach, beforeAll, expect, it } from 'vitest'
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
 
 it('fetches default prompt IDs successfully', async () => {
   const ids = await getDefaultPromptIds()
