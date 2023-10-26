1b228380baba5658dca294607eb583e9029ceaab
Tony Schroder <http://www.tony-schroeder.de>
2023-10-26 13:43:15 +0200

    Redesign Prompts & Entry Points

--- apps/playwright-scraper/src/set-default-prompts.ts
+++ apps/playwright-scraper/src/set-default-prompts.ts
@@ -1,11 +1,34 @@
-import { createPrompt, deleteAllDefaultPrompts } from '@george-ai/strapi-client'
+import { getEntryPoints, updateEntryPoint } from '@george-ai/strapi-client'
 import { defaultPrompts } from './default-prompts'
 
+const isSupportedLanguage = (
+  lang: string,
+): lang is keyof typeof defaultPrompts =>
+  Object.keys(defaultPrompts).includes(lang)
+
 const setDefaultPrompts = async () => {
-  await deleteAllDefaultPrompts()
-  for (const [locale, promptData] of Object.entries(defaultPrompts)) {
-    const { summary, keywords } = promptData
-    await createPrompt(locale, summary, keywords)
+  const entryPoints = await getEntryPoints()
+  const filteredEntryPoints = entryPoints.filter((entryPoint) =>
+    entryPoint.prompts.some((prompt) => prompt.isDefaultPrompt),
+  )
+
+  for (const entryPoint of filteredEntryPoints) {
+    const updatedPrompts = entryPoint.prompts.map((prompt) => {
+      if (prompt.isDefaultPrompt && isSupportedLanguage(prompt.language)) {
+        const defaultPrompt = defaultPrompts[prompt.language]
+        return {
+          id: prompt.id,
+          promptForSummary: JSON.stringify(defaultPrompt.summary),
+          promptForKeywords: JSON.stringify(defaultPrompt.keywords),
+          largeLanguageModel: 'gpt-3.5-turbo',
+          isDefaultPrompt: true,
+          language: prompt.language,
+        }
+      }
+      return prompt
+    })
+
+    await updateEntryPoint(entryPoint.entryPointId, updatedPrompts)
   }
 }
 
