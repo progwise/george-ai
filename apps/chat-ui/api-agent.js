5c040241f960467cb3bd6496ad0840a8a5fe7a43
Michael Vogt <michael.vogt@progwise.net>
2024-07-19 15:14:16 +0200

    added todo comments

--- apps/chat-ui/api-agent.js
+++ apps/chat-ui/api-agent.js
@@ -21,6 +21,34 @@ import { CheerioWebBaseLoader } from 'langchain/document_loaders/web/cheerio'
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 import { MemoryVectorStore } from 'langchain/vectorstores/memory'
 
+//
+// i) Add new tool to make the george ai typesense search on the scraped pages
+// ii) keep the current tavilySearch and CheerioWebBaseLoader tools
+// iii) understand how tools are selected and queried? When does the chatbot agent use which tool?
+// iv) build a configuration route to add and activate configurations for the chatbot
+
+const chatbotConfiguration = {
+  name: 'config 1',
+  systemPrompt: 'Du hilfst Angestellten bei der Suche von Information aus dem Intranet.', // more than 1 prompt??
+  tools: [
+    {
+      type: 'cheerioWebLoader',
+      urls: ['https://www.progwise.net/', 'https://prof.um.ac.ir/akbazar/']
+    }, {
+      type: 'tavilySearchTool'
+    }, {
+      type: 'georgeAiTypesenseSearchTool',
+      // ??
+    }
+  ]
+}
+
+// v) how to build a UI to store several configurations to some DB  and activate one of them ? localhost:5173/configuration
+// vi) clear button for the chat history
+// vii) do not use send button, just send on Return key
+// viii) do it in typescript
+
+
 // Create Retriever
 const urls = ['https://www.progwise.net/', 'https://prof.um.ac.ir/akbazar/']
 
