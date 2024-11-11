a7b83ee9645e190712fd301d6fbdf62fbb1a51e3
Splenda <splenda>
2024-11-11 11:39:01 +0100

    Fixes.

--- apps/chat-ui/api-agent.js
+++ apps/chat-ui/api-agent.js
@@ -1,72 +1,29 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-import {
-  ChatPromptTemplate,
-  MessagesPlaceholder,
-} from '@langchain/core/prompts'
+import { ChatPromptTemplate } from '@langchain/core/prompts'
 import { ChatOpenAI } from '@langchain/openai'
-
-import { AIMessage, HumanMessage } from '@langchain/core/messages'
-import {RunnableMap, RunnablePassthrough} from '@langchain/core/runnables'
-import { AgentExecutor, createOpenAIFunctionsAgent } from 'langchain/agents'
-
-// Tool imports
-import { TavilySearchResults } from '@langchain/community/tools/tavily_search'
-import { createRetrieverTool } from 'langchain/tools/retriever'
+import { formatDocumentsAsString } from 'langchain/util/document'
+import { TavilySearchAPIRetriever } from '@langchain/community/retrievers/tavily_search_api'
+import {
+  RunnableMap,
+  RunnablePassthrough,
+  RunnableLambda,
+  RunnableBranch,
+} from '@langchain/core/runnables'
 
 // Custom Data Source, Vector Stores
 import { OpenAIEmbeddings } from '@langchain/openai'
-import { CheerioWebBaseLoader } from 'langchain/document_loaders/web/cheerio'
+
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 import { MemoryVectorStore } from 'langchain/vectorstores/memory'
-import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 
-import { StringOutputParser } from "@langchain/core/output_parsers";
+import { z } from 'zod'
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf'
 
+const mag_example1 = './data/mag_example1.pdf'
 
-
-//
-// i) Add new tool to make the george ai typesense search on the scraped pages
-// ii) keep the current tavilySearch and CheerioWebBaseLoader tools
-// iii) understand how tools are selected and queried? When does the chatbot agent use which tool?
-// iv) build a configuration route to add and activate configurations for the chatbot
-
-// eslint-disable-next-line no-unused-vars
-// const chatbotConfiguration = {
-//   name: 'config 1',
-//   systemPrompt:
-//     'Du hilfst Angestellten bei der Suche von Information aus dem Intranet.', // more than 1 prompt??
-//   tools: [
-//     {
-//       type: 'cheerioWebLoader',
-//       urls: ['https://www.progwise.net/', 'https://prof.um.ac.ir/akbazar/'],
-//     },
-//     {
-//       type: 'tavilySearchTool',
-//     },
-//     {
-//       type: 'georgeAiTypesenseSearchTool',
-//       // ??
-//     },
-//   ],
-// }
-
-// v) how to build a UI to store several configurations to some DB  and activate one of them ? localhost:5173/configuration
-// vi) clear button for the chat history
-// vii) do not use send button, just send on Return key
-// viii) do it in typescript
-
-// Create Retriever
-// const urls = ['https://www.progwise.net/', 'https://prof.um.ac.ir/akbazar/']
-
-// const loader = new CheerioWebBaseLoader(urls)
-
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-
-const mag_example1 = "./data/mag_example1.pdf";
-
-const loader = new PDFLoader(mag_example1);
+const loader = new PDFLoader(mag_example1)
 
 const docs = await loader.load()
 // console.log(docs[0])
@@ -82,79 +39,186 @@ const embeddings = new OpenAIEmbeddings()
 
 const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, embeddings)
 
-const retriever = vectorStore.asRetriever({
+const retrieverLocal = vectorStore.asRetriever({
   k: 10,
+})
+
+const retrieverWeb = new TavilySearchAPIRetriever({
+  k: 6, // Number of articles to retrieve
 })
 
 // Instantiate the model
 const model = new ChatOpenAI({
-  modelName: 'gpt-3.5-turbo-1106',
+  modelName: 'gpt-4o',
   temperature: 0.2,
 })
 
+// Tool calling setup with structured output
+const modelWithStructuredOutput = model.withStructuredOutput(
+  z.object({
+    answer: z.string().nullable(),
+  }),
+)
+
 // Prompt Template
 const prompt = ChatPromptTemplate.fromMessages([
-  ('system', `You are a helpful assistant.
+  ('system',
+  `You are a helpful assistant.
     Use the following pieces of retrieved context to answer 
     the question. If you don't know the answer, say that you 
     don't know. Use three sentences maximum and keep the
     answer concise.
 
+    State that you cannot answer if there were no data about the question in the context by returning null.
+
+    This is the context:
+
+    {context}
     `),
   //new MessagesPlaceholder('chat_history'),
   ('human', '{input}'),
   //new MessagesPlaceholder('agent_scratchpad'),
 ])
 
-const chain1 = prompt.pipe(model)
+const promptChain = prompt.pipe(modelWithStructuredOutput)
 
 const map1 = RunnableMap.from({
   input: new RunnablePassthrough(),
-  docs: retriever
+  docs: retrieverLocal,
+  // Array,
 })
 
-const chain2 = map1.assign({answer: chain1}).pick(['answer'])
-// const chain2 = createStuffDocumentsChain({
-//   llm: model,
-//   prompt,
-//   outputParser: new StringOutputParser(),}
-// )
-const response = await chain2.invoke('Was muss ich im Verzasca Tal unbedingt ansehen?')
-console.log(response)
+const map2 = RunnableMap.from({
+  input: (input) => input.input,
+  docs: (input) => retrieverWeb.invoke(input.input),
+  // Array,
+})
 
-// Tools
-// const searchTool = new TavilySearchResults()
-// const retrieverTool = createRetrieverTool(retriever, {
-//   name: 'lcel_search',
-//   description:
-//     'Use this tool when searching for information about provided URLs.',
-// })
+const webChain = map2
+  .assign({
+    context: (input) => {
+      // console.log('web', input)
+      return formatDocumentsAsString(input.docs)
+    },
+  })
+  // .assign({
+  //   temp: (input) => {
+  //     console.log('assign', input)
+  //     return 'temp'
+  //   },
+  // })
+  .assign({ answer: promptChain })
+  .assign({
+    context: (data) => {
+      console.log('result of promptchain', data)
+      return data
+    },
+  })
+// .pick(['answer'])
 
-// const tools = [searchTool, retrieverTool]
+const chain2 = map1
+  .assign({
+    context: (map1) => {
+      return formatDocumentsAsString(map1.docs)
+    },
+  })
+  .assign({ answer: promptChain })
+  .assign({
+    context: ({ input, docs, context, answer }) => {
+      //console.log('huhu', Object.keys(data))
+      // return data
+      console.log('local answer', answer.answer)
+      if (answer.answer) {
+        return { input, docs, context, answer }
+      } //Create WebChain and use
+      console.log('asking webChain', { input })
+      // return webChain.invoke(input) //Add the formatDocumentsAsString and the prompt
+      return webChain.invoke(input) //Add the formatDocumentsAsString and the prompt
+    },
+  })
+  //Add another assign that takes Web retriever
 
-// const agent = await createOpenAIFunctionsAgent({
-//   llm: model,
-//   prompt,
-//   tools,
-// })
+  // .assign(({ answer }) => {
+  //   console.log(`Hi`, answer)
+  //   if (answer.answer) {
+  //     return { answer: answer.answer }
+  //   } //Create WebChain and use
+  //   return webChain //Add the formatDocumentsAsString and the prompt
+  //   // ;({ input: new RunnablePassthrough(), docs: retrieverLocal })
+  // })
+  .assign({
+    context: (data) => {
+      console.log('after', Object.keys(data.context))
+      return data.context
+    },
+  })
+  .pick(['answer'])
 
-// Create the executor
-// const agentExecutor = new AgentExecutor({
-//   agent,
-//   tools,
-// })
+const formatDocs = new RunnableLambda({
+  func: (input) => {
+    return {
+      ...input,
+      context: formatDocumentsAsString(input.docs),
+    }
+  },
+})
 
-// const chat_history = []
+const outputChain = new RunnableLambda({
+  func: (input) => {
+    return {
+      answer: input.answerFromPrompt.answer,
+    }
+  },
+})
 
-// export async function handleUserInput(input) {
-//   // agent.
-//   const response = await agentExecutor.invoke({
-//     input: input,
-//     chat_history: chat_history,
+// const debuggerChain = (tag) =>
+//   new RunnableLambda({
+//     func: (input) => {
+//       console.log('debug', tag, input)
+//       return input
+//     },
 //   })
 
-//   chat_history.push(new HumanMessage(input))
-//   chat_history.push(new AIMessage(response.output))
+const webChain2 = map2
+  // .pipe(debuggerChain('after retrieve'))
+  .pipe(formatDocs)
+  // .pipe(debuggerChain('after formatting'))
+  .assign({ answerFromPrompt: promptChain })
+  // .pipe(debuggerChain('after prompt'))
+  .pipe(outputChain)
 
-//   return response.output
-// }
+const chain3 = map1.pipe(formatDocs).pipe(
+  RunnableMap.from({
+    input: (input) => input.input,
+    answerFromPrompt: promptChain,
+  }).pipe(
+    RunnableBranch.from([
+      [
+        (input) => {
+          const noDataInPdfFound = !input.answerFromPrompt.answer
+
+          if (noDataInPdfFound) {
+            console.log("Couldn't find any information in pdf, use web instead")
+          }
+
+          return noDataInPdfFound
+        },
+        webChain2,
+      ],
+      outputChain,
+    ]),
+  ),
+)
+// console.log(await chain3.invoke('Ist Greifswald ein lohnendes Reiseziel?'))
+console.log(
+  await chain3.invoke('Was muss ich in Greifswald unbedingt ansehen?'),
+  // await chain3.invoke('Was muss ich im Verzasca Tal unbedingt ansehen?'),
+)
+
+// const response = await chain2.invoke(
+//   //'Ist Empuriabrava ein lohnendes Reiseziel?',
+//   'Ist Greifswald ein lohnendes Reiseziel?',
+// )
+//const response = await chain2.invoke('Was muss ich im Verzasca Tal unbedingt ansehen?')
+// //const response = await chain2.invoke('Was muss ich mir in Graz unbedingt ansehen?')
+// console.log(`MY:`, response)
