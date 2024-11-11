7dd1485768a853f88def1c56029d4f217866e8ae
Splenda <splenda>
2024-11-11 19:15:07 +0100

    Removed extrac commented code.

--- apps/chat-ui/refactorAgent.js
+++ apps/chat-ui/refactorAgent.js
@@ -94,28 +94,6 @@ const map2 = RunnableMap.from({
   // Array,
 })
 
-//   const webChain = map2
-//   .assign({
-//     context: (input) => {
-//       return formatDocumentsAsString(input.docs)
-//     },
-//   })
-
-//   .assign({ answer: promptChain })
-//   .assign({
-//     context: (data) => {
-//       console.log('result of promptchain', data)
-//       return data
-//     },
-//   })
-//   .assign({
-//     context: (data) => {
-//       console.log('after', Object.keys(data.context))
-//       return data.context
-//     },
-//   })
-//   .pick(['answer'])
-
 const formatDocs = new RunnableLambda({
   func: (input) => {
     return {
