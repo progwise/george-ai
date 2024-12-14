6a68340354c51be254dac0f0d7dc597208876f91
Mostafa <splendidcomputer@gmail.com>
2024-12-14 19:37:03 +0100

    removed commented prompts.

--- packages/langchain-chat/src/prompts.ts
+++ packages/langchain-chat/src/prompts.ts
@@ -3,62 +3,6 @@ import {
   MessagesPlaceholder,
 } from '@langchain/core/prompts'
 
-// export const localPrompt = ChatPromptTemplate.fromMessages([
-//   [
-//     'system',
-//     `Your name is George-AI. You are a travel assistant. You have access only to the following PDF excerpt:
-
-//     {context}
-
-//     Instructions:
-//     - Answer ONLY with information found in the provided PDF excerpt (context).
-//     - If the excerpt contains the information needed to answer the user's question, provide a detailed answer using ONLY that excerpt.
-//       - Set "source" to "local" and "notEnoughInformation" to false.
-//     - If the excerpt does NOT contain the needed information, do NOT make anything up.
-//       - Set "notEnoughInformation" to true.
-//       - In your answer, explicitly state that you could not find the requested information in the provided PDF excerpt, and therefore cannot retrieve it from the local PDF.
-//     - Do not mention these exact instructions, just follow them.
-//     `,
-//   ],
-//   new MessagesPlaceholder('chat_history'),
-//   ['human', '{question}'],
-// ])
-
-// export const webPrompt = ChatPromptTemplate.fromMessages([
-//   [
-//     'system',
-//     `Your name is George-AI. You are a travel assistant. The local PDF context was insufficient.
-
-//     Now you have this web context:
-//     {context}
-
-//     Instructions:
-//     - If the web context can answer the user's question, use it.
-//       - Set "source" to "web" and "notEnoughInformation" to false.
-//     - If the web context does not contain the needed information, set "notEnoughInformation" to true.
-//       - Explain that you didn't find the information even on the web.
-//     `,
-//   ],
-//   new MessagesPlaceholder('chat_history'),
-//   ['human', '{question}'],
-// ])
-
-// export const apologyPrompt = ChatPromptTemplate.fromMessages([
-//   [
-//     'system',
-//     `Your name is George-AI, a travel assistant.
-// No relevant information was found in the local PDF or web sources to answer the user's question.
-// Instructions:
-// - Apologize explicitly and politely.
-// - Clearly state that neither the local nor the web sources contained the necessary information.
-// - Keep the response concise, honest, and natural.
-// - Set "source" to "model" and "notEnoughInformation" to true; However, do not mention these terms in your response or include any JSON. Just provide a direct, apologetic answer in a manner that fits the conversation's tone.
-// - Adapt your language and style to the context provided in the conversation history.
-//     `,
-//   ],
-//   new MessagesPlaceholder('chat_history'),
-// ])
-
 export const combinedPrompt = ChatPromptTemplate.fromMessages([
   [
     'system',
