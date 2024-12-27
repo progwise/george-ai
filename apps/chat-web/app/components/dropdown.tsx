32f99d3030bd7e02bc87db0775eac371c440f12e
Mostafa Razavi <MostafaRazavi@users.noreply.github.com>
2024-12-27 15:50:56 +0000

    Close dropdown after click

--- apps/chat-web/app/components/dropdown.tsx
+++ apps/chat-web/app/components/dropdown.tsx
@@ -1,24 +1,43 @@
+import { useState } from 'react'
+
 interface DropdownProps {
   title: string
   options: Array<{ title: string; action: () => void }>
 }
+
 export const Dropdown = (props: DropdownProps): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOptionClick = (action: () => void) => {
+    action()
+    setIsOpen(false)
+  }
+
   return (
     <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn m-1">
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn m-1"
+        onClick={() => setIsOpen(!isOpen)}
+      >
         {props.title}
       </div>
-      <ul
-        tabIndex={0}
-        className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
-      >
-        {props.options.map((option, index) => (
-          // eslint-disable-next-line @eslint-react/no-array-index-key
-          <li key={index}>
-            <div onClick={() => option.action()}>{option.title}</div>
-          </li>
-        ))}
-      </ul>
+      {isOpen && (
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+        >
+          {props.options.map((option, index) => (
+            // eslint-disable-next-line @eslint-react/no-array-index-key
+            <li key={index}>
+              <div onClick={() => handleOptionClick(option.action)}>
+                {option.title}
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
