d3ef376dcedac97be207d40c13010ff4a50863d6
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2025-03-18 11:26:04 +0000

    Reprocess with error handling

--- packages/pothos-graphql/src/graphql/ai-library-file/index.ts
+++ packages/pothos-graphql/src/graphql/ai-library-file/index.ts
@@ -172,3 +172,59 @@ builder.mutationField('dropFile', (t) =>
     },
   }),
 )
+
+builder.mutationField('reProcessFile', (t) =>
+  t.prismaField({
+    type: 'AiLibraryFile',
+    args: {
+      fileId: t.arg.string({ required: true }),
+    },
+    resolve: async (query, _source, { fileId }) => {
+      const file = await prisma.aiLibraryFile.findUnique({
+        ...query,
+        where: { id: fileId },
+      })
+      if (!file) {
+        throw new Error(`File not found: ${fileId}`)
+      }
+
+      await prisma.aiLibraryFile.update({
+        where: { id: fileId },
+        data: {
+          processingStartedAt: new Date(),
+        },
+      })
+
+      try {
+        const embeddedFile = await embedFile(file.libraryId, {
+          id: file.id,
+          name: file.name,
+          originUri: file.originUri!,
+          mimeType: file.mimeType,
+          path: getFilePath(file.id),
+        })
+
+        return await prisma.aiLibraryFile.update({
+          ...query,
+          where: { id: fileId },
+          data: {
+            ...embeddedFile,
+            processedAt: new Date(),
+            processingEndedAt: new Date(),
+            processingErrorAt: null,
+            processingErrorMessage: null,
+          },
+        })
+      } catch (error) {
+        await prisma.aiLibraryFile.update({
+          where: { id: fileId },
+          data: {
+            processingErrorAt: new Date(),
+            processingErrorMessage: (error as Error).message,
+          },
+        })
+        throw error
+      }
+    },
+  }),
+)
