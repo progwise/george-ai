31eb77305d752c5b8da0b4297c77487789c82959
Michael Vogt <michael.vogt@progwise.net>
2025-04-09 09:07:16 +0000

    review and cleanup

--- apps/chat-web/app/routes/api/process-documents.ts
+++ apps/chat-web/app/routes/api/process-documents.ts
@@ -1,12 +1,9 @@
 import { json } from '@tanstack/react-start'
 import { createAPIFileRoute } from '@tanstack/react-start/api'
 
-import { processUnprocessedDocuments } from '@george-ai/langchain-chat'
-
 export const APIRoute = createAPIFileRoute('/api/process-documents')({
   GET: ({ request, params }) => {
     console.log('GET /api/process-documents', { request, params })
-    setTimeout(() => processUnprocessedDocuments(), 2000)
-    return json({ message: 'Hello "/api/process-documents"!' })
+    return json({ message: 'Hello "/api/process-documents" is not implemented!' })
   },
 })
