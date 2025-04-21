745cf6bc5bddf7b9f8116263fbc01d4579d4f558
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2025-04-21 18:53:27 +0000

    Adjust toaster sizes

--- apps/chat-web/app/components/georgeToaster.tsx
+++ apps/chat-web/app/components/georgeToaster.tsx
@@ -17,13 +17,13 @@ export const GeorgeToaster = () => {
   )
 }
 
-const defaultAlertClasses = 'alert w-auto cursor-pointer'
+const defaultAlertClasses = 'alert w-auto cursor-pointer text-sm'
 
 export const toastError = (message: string) => {
   return toast.custom(
     (t) => (
       <div className={twMerge(defaultAlertClasses, 'alert-error')} onClick={() => toast.remove(t.id)}>
-        <ErrorIcon className="size-6" />
+        <ErrorIcon className="size-5" />
         <span>{message}</span>
       </div>
     ),
@@ -34,7 +34,7 @@ export const toastError = (message: string) => {
 export const toastSuccess = (message: string) => {
   return toast.custom((t) => (
     <div className={twMerge(defaultAlertClasses, 'alert-success')} onClick={() => toast.remove(t.id)}>
-      <CheckIcon />
+      <CheckIcon className="size-5" />
       <span>{message}</span>
     </div>
   ))
