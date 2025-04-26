4b9c0d6407cd128882ee27aaee4dd88181debdc1
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2025-04-26 02:16:02 +0000

    Modify toasters

--- apps/chat-web/app/components/georgeToaster.tsx
+++ apps/chat-web/app/components/georgeToaster.tsx
@@ -2,6 +2,7 @@ import toast, { Toaster } from 'react-hot-toast'
 import { twMerge } from 'tailwind-merge'
 
 import { CheckIcon } from '../icons/check-icon'
+import { CrossIcon } from '../icons/cross-icon'
 import ErrorIcon from '../icons/error-icon'
 
 export const GeorgeToaster = () => {
@@ -16,15 +17,22 @@ export const GeorgeToaster = () => {
     />
   )
 }
-
-const defaultAlertClasses = 'alert w-auto cursor-pointer text-sm py-2 px-2'
+const defaultAlertClasses = 'alert flex w-auto cursor-pointer items-start gap-1 py-2 relative text-sm rounded-md'
 
 export const toastError = (message: string) => {
   return toast.custom(
-    (t) => (
-      <div className={twMerge(defaultAlertClasses, 'alert-error')} onClick={() => toast.remove(t.id)}>
+    (toastInstance) => (
+      <div className={twMerge(defaultAlertClasses, 'alert-error')}>
         <ErrorIcon className="size-5" />
-        <span>{message}</span>
+        <span className="break-words pr-4 text-sm">{message}</span>
+        <button
+          type="button"
+          className="tooltip tooltip-left absolute right-0 top-0 m-1"
+          data-tip="Close"
+          onClick={() => toast.remove(toastInstance.id)}
+        >
+          <CrossIcon className="size-4" />
+        </button>
       </div>
     ),
     { ariaProps: { role: 'alert', 'aria-live': 'assertive' } },
@@ -32,10 +40,18 @@ export const toastError = (message: string) => {
 }
 
 export const toastSuccess = (message: string) => {
-  return toast.custom((t) => (
-    <div className={twMerge(defaultAlertClasses, 'alert-success')} onClick={() => toast.remove(t.id)}>
+  return toast.custom((toastInstance) => (
+    <div className={twMerge(defaultAlertClasses, 'alert-success')}>
       <CheckIcon className="size-5" />
-      <span>{message}</span>
+      <span className="break-words pr-4 text-sm">{message}</span>
+      <button
+        type="button"
+        className="tooltip tooltip-left absolute right-0 top-0 m-1"
+        data-tip="Close"
+        onClick={() => toast.remove(toastInstance.id)}
+      >
+        <CrossIcon className="size-4" />
+      </button>
     </div>
   ))
 }
