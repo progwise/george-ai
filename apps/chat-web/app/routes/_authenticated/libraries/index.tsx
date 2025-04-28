4f56e5d563cec08b9d3adaaec18da321884638e6
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2025-04-28 14:44:45 +0000

    Library page loading

--- apps/chat-web/app/routes/_authenticated/libraries/index.tsx
+++ apps/chat-web/app/routes/_authenticated/libraries/index.tsx
@@ -5,6 +5,7 @@ import { dateStringShort, timeString } from '@george-ai/web-utils'
 
 import { getLibrariesQueryOptions } from '../../../components/library/get-libraries-query-options'
 import { LibraryNewDialog } from '../../../components/library/library-new-dialog'
+import { LoadingSpinner } from '../../../components/loading-spinner'
 import { useTranslation } from '../../../i18n/use-translation-hook'
 
 export const Route = createFileRoute('/_authenticated/libraries/')({
@@ -18,8 +19,11 @@ function RouteComponent() {
   const { user } = Route.useRouteContext()
   const navigate = useNavigate()
   const { data, isLoading } = useSuspenseQuery(getLibrariesQueryOptions(user.id))
-
   const { t, language } = useTranslation()
+
+  if (isLoading) {
+    return <LoadingSpinner />
+  }
 
   return (
     <article className="flex w-full flex-col gap-4">
