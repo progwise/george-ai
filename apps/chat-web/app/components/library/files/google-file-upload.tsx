969514ffeb332a15e9e40f6c2383709887121c5d
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2025-06-18 10:05:27 +0000

    Tooltip fix

--- apps/chat-web/app/components/library/files/google-file-upload.tsx
+++ apps/chat-web/app/components/library/files/google-file-upload.tsx
@@ -37,7 +37,7 @@ export const GoogleFileUploadButton = ({ libraryId, disabled }: GoogleFileUpload
       <button
         type="button"
         className="btn btn-xs tooltip tooltip-bottom"
-        data-tip={t('tooltips.addGoogleDriveFiles')}
+        data-tip={t('texts.addGoogleDriveFiles')}
         onClick={() => {
           if (googleDriveAccessToken) {
             dialogRef.current?.showModal()
