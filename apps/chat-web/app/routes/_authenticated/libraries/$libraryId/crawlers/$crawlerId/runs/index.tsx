776f8a1e6ac1a1883a2a1e0397e6774eda922406
Michael Vogt <michael.vogt@progwise.net>
2025-07-22 06:19:28 +0000

    fix: last run navigate

--- apps/chat-web/app/routes/_authenticated/libraries/$libraryId/crawlers/$crawlerId/runs/index.tsx
+++ apps/chat-web/app/routes/_authenticated/libraries/$libraryId/crawlers/$crawlerId/runs/index.tsx
@@ -1,6 +1,5 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
-import { useEffect } from 'react'
 
 import { getCrawlerQueryOptions } from '../../../../../../../components/library/crawler/get-crawler'
 import { RunCrawlerButton } from '../../../../../../../components/library/crawler/run-crawler-button'
@@ -21,14 +20,13 @@ function RouteComponent() {
     data: { aiLibraryCrawler: crawler },
   } = useSuspenseQuery(getCrawlerQueryOptions(params))
 
-  useEffect(() => {
-    if (crawler.lastRun) {
-      navigate({
-        to: '/libraries/$libraryId/crawlers/$crawlerId/runs/$crawlerRunId',
-        params: { crawlerRunId: crawler.lastRun.id },
-      })
-    }
-  }, [crawler.lastRun, navigate, params])
+  if (crawler.lastRun) {
+    navigate({
+      to: '/libraries/$libraryId/crawlers/$crawlerId/runs/$crawlerRunId',
+      params: { crawlerRunId: crawler.lastRun.id },
+    })
+    return null
+  }
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
       <figure>
