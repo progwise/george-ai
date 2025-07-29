6e14e02c3e723b984c7c016de242e08a1bad02be
Michael Vogt <michael.vogt@progwise.net>
2025-07-29 08:08:21 +0000

    cleanup $libraryId/index

--- apps/chat-web/app/routes/_authenticated/libraries/$libraryId/index.tsx
+++ apps/chat-web/app/routes/_authenticated/libraries/$libraryId/index.tsx
@@ -1,11 +1,8 @@
-import { useMutation, useSuspenseQuery } from '@tanstack/react-query'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 
-import { getLibrariesQueryOptions } from '../../../../components/library/get-libraries'
 import { getLibraryQueryOptions } from '../../../../components/library/get-library'
 import { LibraryForm } from '../../../../components/library/library-form'
-import { updateLibrary } from '../../../../components/library/update-library'
-import { LoadingSpinner } from '../../../../components/loading-spinner'
 
 export const Route = createFileRoute('/_authenticated/libraries/$libraryId/')({
   component: RouteComponent,
@@ -15,7 +12,6 @@ export const Route = createFileRoute('/_authenticated/libraries/$libraryId/')({
 })
 
 function RouteComponent() {
-  const { queryClient } = Route.useRouteContext()
   const { libraryId } = Route.useParams()
 
   const { data: aiLibrary } = useSuspenseQuery(getLibraryQueryOptions(libraryId))
