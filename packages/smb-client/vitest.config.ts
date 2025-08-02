71e9cad20ca6a5c1154c254878812b7c2bf7bbcc
Michael Vogt <michael.vogt@progwise.net>
2025-08-02 11:56:26 +0000

    cleanup

--- packages/smb-client/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'node',
-    testTimeout: 30000, // 30 seconds for SMB operations
-  },
-})
