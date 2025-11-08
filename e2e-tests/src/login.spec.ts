8859336cb82808a9f9349b2a2ebcc574feccbb46
Michael Vogt <michael.vogt@progwise.net>
2025-11-08 18:44:24 +0000

    repair login test

--- e2e-tests/src/login.spec.ts
+++ e2e-tests/src/login.spec.ts
@@ -5,7 +5,7 @@ const E2E_PASSWORD = process.env.E2E_PASSWORD!
 
 test('login', async ({ page }) => {
   await page.goto('/')
-  await page.getByRole('button', { name: 'Sign in', exact: true }).click()
+  await page.getByRole('button', { name: 'Sign in', exact: false }).click()
 
   // Keycloak login
   await page.getByRole('textbox', { name: 'Username or email' }).fill(E2E_USERNAME)
